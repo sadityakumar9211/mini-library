65f7d6a888195fd6decc7d90b2471759e40d3df8
Aditya Kumar Singh <sadityakumar9211@gmail.com>
2022-10-02 14:26:24 +0530

    fixed ui

--- components/Header.js
+++ components/Header.js
@@ -5,11 +5,11 @@ export default function Header() {
         <nav className="p5 border-b-2 flex flex-row justify-between items-center">
             <h1 className="py-4 px-4 font-bold text-3xl"><Link href="/">Library</Link></h1>
             <div className="flex flex-row items-center ">
-                <Link href="/books"><span className="mr-4 p-6">Books</span>
+                <Link href="/books"><a className="mr-4 p-6">Books</a>
                 </Link>
-                <Link href="/magazine"><span className="mr-4 p-6">Magazine</span>
+                <Link href="/magazine"><a className="mr-4 p-6">Magazine</a>
                 </Link>
-                <Link href="/author"><span className="mr-4 p-6">Author</span>
+                <Link href="/author"><a className="mr-4 p-6">Author</a>
                 </Link>
             </div>
         </nav>
