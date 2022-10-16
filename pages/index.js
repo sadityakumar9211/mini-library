20b2feda40cb430b5cadcbd884eed21ed385801f
Raj <varasaniraj5@gmail.com>
2022-10-16 19:56:14 +0530

    improve navbar interface - spacing and logo size - update letter spacing for logo and title in index

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,9 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <div className="flex flex-col justify-center pt-12 content-center h-screen">
-      <h1 className={`text-lg font-bold text-center ${styles.title}`}>
+      <h1
+        className={`text-lg font-bold text-center tracking-tight ${styles.title}`}
+      >
         Welcome to Library
       </h1>
       <h2 className={`text-md font-semibold text-center ${styles.subtitle}`}>
