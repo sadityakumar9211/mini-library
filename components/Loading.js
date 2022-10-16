ea68a244c8450d221f3f4d9a260e61e8ac7e7eee
Raj <varasaniraj5@gmail.com>
2022-10-16 20:32:33 +0530

    fix: update `class` with `className` prop

--- components/Loading.js
+++ components/Loading.js
@@ -3,7 +3,7 @@ export default function Loading() {
     <div className="p-10 w-screen h-80 flex justify-center items-center">
       <svg
         aria-hidden="true"
-        class="mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+        className="mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
