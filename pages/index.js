4ef3bc41291b4926fdea205d92f2a4e4dacdc2fc
Raj <varasaniraj5@gmail.com>
2022-10-16 15:52:35 +0530

    Update home page design

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,13 @@ import { useState } from "react";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
-
 export default function Home() {
   return (
     <div className="flex flex-col justify-center pt-12 content-center">
-      <h1 className="text-lg font-bold text-center">Welcome to Library</h1>
-      <h2 className="text-md font-semibold text-center">
+      <h1 className={`text-lg font-bold text-center ${styles.title}`}>
+        Welcome to Library
+      </h1>
+      <h2 className={`text-md font-semibold text-center ${styles.subtitle}`}>
         You can browse through the books and magazines and details of authors.
       </h2>
     </div>
