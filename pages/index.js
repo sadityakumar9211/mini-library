1328acbb99d508505cadb2c99b3a65da8bec2f78
Raj <varasaniraj5@gmail.com>
2022-10-17 10:45:50 +0530

    Add image to home page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import LibImage from "../assets/library.jpg";
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center pt-12 content-center h-screen">
+    <div className="flex flex-col justify-center pt-12 content-center min-h-80">
       <h1
         className={`text-lg font-bold text-center tracking-tight ${styles.title}`}
       >
@@ -13,6 +14,9 @@ export default function Home() {
       <h2 className={`text-md font-semibold text-center ${styles.subtitle}`}>
         You can browse through the books and magazines and details of authors.
       </h2>
+      <div className={styles.imageWrapper}>
+        <Image src={LibImage} layout="intrinsic" />
+      </div>
     </div>
   );
 }
