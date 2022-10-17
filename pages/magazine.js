910b8f80d1453e94864c4d8ce2390dd5f934408a
Raj <varasaniraj5@gmail.com>
2022-10-17 10:53:48 +0530

    Add Header title in Magazines page

--- pages/magazine.js
+++ pages/magazine.js
@@ -34,73 +34,78 @@ export default function Magazines() {
   return isLoading ? (
     <Loading />
   ) : (
-    <div>
-      <select
-        className="select w-auto max-w-xs my-3 mb-2"
-        onChange={(event) => {
-          setTextType(event.target.value);
-        }}
-        value={textType}
-      >
-        <option selected>Search by</option>
+    <>
+      <h1 className="m-5 mx-4 text-3xl font-semibold tracking-tighter">
+        Magazines
+      </h1>
+      <div>
+        <select
+          className="select w-auto max-w-xs my-3 mb-2"
+          onChange={(event) => {
+            setTextType(event.target.value);
+          }}
+          value={textType}
+        >
+          <option selected>Search by</option>
 
-        <option value="isbn">ISBN</option>
+          <option value="isbn">ISBN</option>
 
-        <option value="email">author email</option>
-      </select>
-      <input
-        type="text"
-        placeholder={`search by ${textType}`}
-        className="input input-bordered w-full max-w-xs"
-        onChange={(event) => {
-          setSearchText(event.target.value);
-        }}
-        value={searchText}
-        disabled={textType === "email" || textType === "isbn" ? false : true}
-      />
-      {
-        <div className="overflow-x-auto">
-          <table className="table w-full">
-            <thead>
-              <tr>
-                <th></th>
-                <th>Title</th>
-                <th>ISBN</th>
-                <th>Authors</th>
-                <th>Published on</th>
-              </tr>
-            </thead>
-            <tbody>
-              {CSVData.filter((data) => {
-                if (textType === "" || searchText.trim() == "") {
-                  return data;
-                } else if (textType === "isbn") {
-                  return (
-                    data.isbn && data.isbn.includes(searchText.toLowerCase())
-                  );
-                } else if (textType === "email") {
-                  return (
-                    data.authors &&
-                    data.authors.includes(searchText.toLowerCase())
-                  );
-                }
-              })
-                .sort(compare_to_sort)
-                .map((data, index) => {
-                  return data.isbn ? (
-                    <tr>
-                      <th>{index}</th>
-                      <td>{data.title}</td>
-                      <td>{data.isbn}</td>
-                      <td>{data.authors.replace(/,/g, ", ")}</td>
-                      <td>{data.publishedAt}</td>
-                    </tr>
-                  ) : null;
-                })}
-            </tbody>
-          </table>
-        </div>
-      }
-    </div>
+          <option value="email">author email</option>
+        </select>
+        <input
+          type="text"
+          placeholder={`search by ${textType}`}
+          className="input input-bordered w-full max-w-xs"
+          onChange={(event) => {
+            setSearchText(event.target.value);
+          }}
+          value={searchText}
+          disabled={textType === "email" || textType === "isbn" ? false : true}
+        />
+        {
+          <div className="overflow-x-auto">
+            <table className="table w-full">
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>Title</th>
+                  <th>ISBN</th>
+                  <th>Authors</th>
+                  <th>Published on</th>
+                </tr>
+              </thead>
+              <tbody>
+                {CSVData.filter((data) => {
+                  if (textType === "" || searchText.trim() == "") {
+                    return data;
+                  } else if (textType === "isbn") {
+                    return (
+                      data.isbn && data.isbn.includes(searchText.toLowerCase())
+                    );
+                  } else if (textType === "email") {
+                    return (
+                      data.authors &&
+                      data.authors.includes(searchText.toLowerCase())
+                    );
+                  }
+                })
+                  .sort(compare_to_sort)
+                  .map((data, index) => {
+                    return data.isbn ? (
+                      <tr>
+                        <th>{index}</th>
+                        <td>{data.title}</td>
+                        <td>{data.isbn}</td>
+                        <td>{data.authors.replace(/,/g, ", ")}</td>
+                        <td>{data.publishedAt}</td>
+                      </tr>
+                    ) : null;
+                  })}
+              </tbody>
+            </table>
+          </div>
+        }
+      </div>
+    </>
   );
 }
